9e8bf28ac2bfc690d598cb41adef63a92151d6f3
JoniKanoni <jonasbottjer@mailbox.org>
2021-12-15 16:29:33 +0100

    Update hopper.py

--- hopfield/hopper.py
+++ hopfield/hopper.py
@@ -8,15 +8,20 @@ def make_pat():
     Macht 3 binäre Muster
     '''
     
-    N = 16
+    N = 64
+    n = int( np.sqrt(N))
+
 
     np.random.seed(1234)
-    pat1 = np.reshape(np.random.randint(2, size=N) ,  (4,4))
+    pat1 = np.reshape(np.random.randint(2, size=N) ,  (n,n))
 
     np.random.seed(12345)
-    pat2 = np.reshape(np.random.randint(2, size=N) ,  (4,4))
+    pat2 = np.reshape(np.random.randint(2, size=N) ,  (n,n))
 
     np.random.seed(123456)
-    pat3 = np.reshape(np.random.randint(2, size=N) ,  (4,4))
+    pat3 = np.reshape(np.random.randint(2, size=N) ,  (n,n))
     return pat1, pat2, pat3
-make_pat()
+p1, p2, p3 =  make_pat()
+
+plt.imshow(p3)
+plt.show()
