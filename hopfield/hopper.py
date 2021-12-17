cfd206f9938fd6ad736375aecf895812f5935a5a
JoniKanoni <jonasbottjer@mailbox.org>
2021-12-17 12:23:00 +0100

    it's almost lit
    hemming fehlt und maybe anderes updaten auch noch implementieren PogO

--- hopfield/hopper.py
+++ hopfield/hopper.py
@@ -1,61 +1,44 @@
 import numpy as np
 import matplotlib.pyplot as plt
+import copy
 
 
-N = 32
 def make_pat(n):
     '''
-    Macht 3 binäre Muster
+    Macht 3 binäre Muster als Vektor
+    Seeds kann man enablen um die reproduzierbarkeit zu checken, ist aber die doofe Version der Seeds.. 
+    Deswegen lieber ausmachen, weil diese Art von Seeds dann doch manchmal global sind und das Shufflen hinten beeinflussen können..
+    Idk nunmp
+    
     '''
     
-    np.random.seed(1234)
-    pat1 = np.reshape(np.random.randint(2, size=n**2) ,  (n,n))
+    #np.random.seed(1234)
+    pat1 = np.random.randint(2, size=n**2) 
     pat1 = np.where(pat1==0, -1, pat1)
 
-    np.random.seed(4321)
-    pat2 = np.reshape(np.random.randint(2, size=n**2) ,  (n,n))
+    #np.random.seed(4321)
+    pat2 = np.random.randint(2, size=n**2)
     pat2 = np.where(pat2==0, -1 , pat2)
 
-    np.random.seed(7777)
-    pat3 = np.reshape(np.random.randint(2, size=n**2) ,  (n,n))
+    #np.random.seed(7777)
+    pat3 = np.random.randint(2, size=n**2)
     pat3 = np.where(pat3==0, -1, pat3)
     return pat1, pat2, pat3
-p1, p2, p3 =  make_pat(N)
-
-all = (p1,p2,p3)
 
 def weights(n, patterns):
     '''
     initial weights
     56 connections ungleich 0 bei N=8, da keine Kopplung mit sich selbst aka Diagonale=0
     '''   
-    w_init = np.zeros((n,n)) 
+    w_init = np.zeros((64,64)) 
 
     # matrixmultiplikation und so für die weights
+    
     for x in patterns:
-        w_init +=  np.outer(x[0], x[0]) / n    
+        w_init +=     np.outer(x,x)  #np.matmul(x, x)     #np.dot(x, x) / n    
     np.fill_diagonal(w_init, 0)     #W_ii = 0
-
+    
     return w_init
-
-w = weights(N, all)
-
-print(np.shape(w))
-
-bias = np.sum(all, axis=0) / len(all)
-print(np.shape(bias))
-def energy(patterns, W):
-    '''
-    Energie.. falls man asynchron sein will
-    '''
-    Energy = []
-    for x in patterns:
-        hmm = -1/2 * np.matmul(x, W)
-        Energy.append(hmm)
-
-    return Energy
-
-e = energy(all, w)
 
 def breaker(pat):
     '''
@@ -65,31 +48,64 @@ def breaker(pat):
     pp = np.copy(pat)
     np.random.shuffle(pp)
     return pp
-pag = breaker(p2)
 
 def recaller(inp, W,  iterations, bias):
     '''
     Vergleicht und updatet
+    hmm braucht man eigentlich nicht, aber ich habs ein bisschen zu meiner eigenen Sicherheit eingeführt
     '''
     hmm = inp
     for ii in range(iterations):
-        new_pos = np.matmul(hmm, W) 
-        hmm = np.sign(new_pos)
+        new_pos = -np.matmul(hmm, W)  -bias 
+        hmm = -np.sign(new_pos)
     return hmm
 
-membered = recaller(p2, w, 100000, bias)
+N = 8
 
-plt.subplot(311)
-plt.ylabel('original')
-plt.legend(str(np.sum(p2)))
-plt.imshow(p2)
-plt.subplot(312)
-plt.ylabel('scuffed')
-plt.legend(str(np.sum(pag)))
-plt.imshow(pag)
-plt.subplot(313)
-plt.ylabel('remembered')
-plt.legend(str(np.sum(membered)))
-plt.imshow(membered)
+p1, p2, p3 =  make_pat(N)
+
+all = (p1,p2,p3)
+
+w = weights(N, all)
+
+bias = np.sum(all, axis=0) / len(all)
+
+pag = breaker(p1)
+
+membered = recaller(pag, w, 10, bias)
+
+# hier nur reshape damit schönere Plots
+p1 = np.reshape(p1, (N,N))
+p2 = np.reshape(p2, (N,N))
+p3 = np.reshape(p3, (N,N))
+pag = np.reshape(pag, (N,N))
+membered = np.reshape(membered, (N,N))
+
+
+# plots
+fig, axs = plt.subplots(2,3, constrained_layout=True)
+
+axs[0,0].imshow(p1)
+axs[0,0].set_ylabel('Pattern 1')
+axs[0,0].tick_params(axis='both', length=0, width=0, labelsize=0)
+axs[0,1].imshow(p2)
+axs[0,1].set_ylabel('Pattern 2')
+axs[0,1].tick_params(axis='both', length=0, width=0, labelsize=0)
+axs[0,2].imshow(p3)
+axs[0,2].set_ylabel('Pattern 3')
+axs[0,2].tick_params(axis='both', length=0, width=0, labelsize=0)
+
+axs[1,0].imshow(w)
+axs[1,0].set_ylabel('Weights')
+axs[1,0].tick_params(axis='both', length=0, width=0, labelsize=0)
+
+axs[1,1].imshow(pag)
+axs[1,1].set_ylabel('Input Pattern')
+axs[1,1].tick_params(axis='both', length=0, width=0, labelsize=0)
+
+axs[1,2].imshow(membered)
+axs[1,2].set_ylabel('Remembered Pattern')
+axs[1,2].tick_params(axis='both', length=0, width=0, labelsize=0)
+
 
 plt.show()
